0ac24d138d98c840084b0c569d27c965c943b64e
Stephen <67360857+sebuckley@users.noreply.github.com>
2022-08-18 07:45:08 +0100

    my solution written

--- src/site/markdown/katas/ParkingBill/index.js
+++ src/site/markdown/katas/ParkingBill/index.js
@@ -1,12 +1,68 @@
-class ParkingBill {
+function ParkingBill(string) {
 
+    if(string === ""){
 
+        throw new TypeError("please enter correct start and end time");
 
+    }
 
+    let splitString = string.split(",");
 
+    if(splitString.length === 1){
 
+        return 100;
+
+    }else{
+
+        const entryTimeHour = splitString[0].split(":")[0];
+        const entryTimeMin = splitString[0].split(":")[1];
+        const exitTimeHour = splitString[1].split(":")[0];
+        const exitTimeMin = splitString[1].split(":")[1];
+        
+        let totalCharge = 0;
+
+        let hourDiff = parseInt(exitTimeHour) - parseInt(entryTimeHour);
+        let minDiff = parseInt(exitTimeMin) - parseInt(entryTimeMin);
+
+        if(hourDiff === 0 && minDiff <= 5){
+
+            totalCharge = 0;
+
+        }else{
+
+            if (minDiff > 0){
+
+                hourDiff += 1;
+
+            }
+
+            if (hourDiff === 1) {
+
+                totalCharge = 2;
+
+            } else if (hourDiff > 1 && hourDiff <= 4) {
+
+                hourDiff = hourDiff - 1;
+                let hour1 = 2;
+
+                totalCharge = hour1 + (hourDiff * 3);
+
+            } else if (hourDiff > 4) {
+
+                hourDiff = hourDiff - 4;
+                let hour1 = 2;
+                let hour2 = 3 * 3;
+
+                totalCharge = hour1 + hour2 + (hourDiff * 5);
+
+            }
+
+        }
+
+        return totalCharge;
+
+    }
 
 }
-
 
 module.exports = { ParkingBill };
--- src/site/markdown/katas/ParkingBill/index.test.js
+++ src/site/markdown/katas/ParkingBill/index.test.js
@@ -1,2 +1,55 @@
 import { ParkingBill } from "./";
 
+describe("Parking bill", () => {
+
+
+    it("a string is input and it returns a number", () => {
+
+        let input = ParkingBill("09:00,10:00");
+        expect(typeof input).toEqual('number');
+
+    });
+
+    it("it accepts a string with comma seperated values HH:MM throws an error is not", () => {
+
+        expect(() => { ParkingBill("") }).toThrow('please enter correct start and end time');
+
+    });
+
+    it("if only an exit time is presented a fee of 100 is charged", () => {
+
+        let input = ParkingBill("10:00");
+        expect(input).toEqual(100);
+
+    });
+
+    it("if entry time and exit time is 09:00,09:05, 5 minutes or less, a fee of 0 returned", () => {
+
+        let input = ParkingBill("09:00,09:05");
+        expect(input).toEqual(0);
+
+    });
+
+    it.each([["09:00,09:10", 2],["09:00,10:00", 2]])("if entry time,exit time %s a fee of %i is returned", (input, expected) => {
+
+        expect(ParkingBill(input)).toEqual(expected);
+
+    });
+
+    it.each([["09:00,10:10", 5],["09:00,13:00", 11]])("if entry time,exit time %s a fee of %i is returned", (input, expected) => {
+
+        expect(ParkingBill(input)).toEqual(expected);
+
+    });
+
+    it.each([["09:00,14:10", 21],["09:00,18:00", 36]])("if entry time,exit time %s a fee of %i is returned", (input, expected) => {
+
+        expect(ParkingBill(input)).toEqual(expected);
+
+    });
+
+
+
+    
+
+})
